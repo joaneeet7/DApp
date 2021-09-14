272fd703392155b34c18d09e214d642ae3978801
Joan Amengual <joan@consilientlabs.com>
2021-09-14 18:10:45 +0200

    Update ganache.sol

--- BLOQUE 1 - GANACHE/Primeros pasos en Ganache/ganache.sol
+++ BLOQUE 1 - GANACHE/Primeros pasos en Ganache/ganache.sol
@@ -3,33 +3,30 @@ pragma solidity ^0.8.0;
 
 contract ganache {
     
+    // Variables iniciales
     string message = "";
-    event newMessage(string message);
     uint [] list;
     
-    // Publicar un mensaje en la cadena
-    function setMessage(string memory _message) public{
+    // Publicar un mensaje en la cadena de bloques
+    function setMessage(string memory _message) public {
         message = _message;
-        emit newMessage(_message);
     }
     
-    // Visualizar el mensaje de la cadena
-    function getMessage() public view returns (string memory) {
+    // Visualizar el mensaje de la cadena de bloques 
+    function getMessage() public view returns (string memory) { 
         return message;
     }
     
-    // Visualizar el gasto de gas en aumento
+    // Aumentar el gasto de gas 
     function fullGas() public {
-        for (uint i = 0; i < 100; i++){
+        for (uint i = 0; i<100; i++){
             list.push(i);
         }
-        
     }
     
-    // Visualizar los parametros guardados
-    function seeGas() public view returns (uint [] memory){
+    // Visualizar los valores que consumen mucho gas
+    function seeValues() public view returns (uint [] memory) {
         return list;
     }
-    
-    
+       
 }
