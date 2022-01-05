7d83b48d86ccbf61fbad3961f43bba85c2f163bb
Joan Amengual <jamengual150899@gmail.com>
2022-01-05 14:39:55 +0100

    Updates

---
+++ precache-manifest.d991e55ec5ddc958b5ca42553b8ec16a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "698ea745b4ea7dd81317",
+    "url": "/DApp/static/css/main.718ed425.chunk.css"
+  },
+  {
+    "revision": "698ea745b4ea7dd81317",
+    "url": "/DApp/static/js/main.698ea745.chunk.js"
+  },
+  {
+    "revision": "49970d8b5d29131a639f",
+    "url": "/DApp/static/css/1.52e735da.chunk.css"
+  },
+  {
+    "revision": "49970d8b5d29131a639f",
+    "url": "/DApp/static/js/1.49970d8b.chunk.js"
+  },
+  {
+    "revision": "4e96a7a0517e2b991394",
+    "url": "/DApp/static/js/runtime~main.4e96a7a0.js"
+  },
+  {
+    "revision": "fb8c844ab16b9edb99bf030b7ac3c6b5",
+    "url": "/DApp/index.html"
+  }
+];
